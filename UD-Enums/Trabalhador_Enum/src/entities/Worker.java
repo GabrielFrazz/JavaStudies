fe4e6bb9ca6a970f4e7571fff39a1b71ab5ab2ef
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2022-11-24 14:25:55 -0300

    almost ready, income not working properly yet

---
+++ UD-Enums/Trabalhador_Enum/src/entities/Worker.java
@@ -0,0 +1,111 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker(){
+
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department){
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return WorkerLevel return the level
+     */
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level the level to set
+     */
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    /**
+     * @return Double return the baseSalary
+     */
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    /**
+     * @param baseSalary the baseSalary to set
+     */
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    /**
+     * @return Department return the department
+     */
+    public Department getDepartment() {
+        return department;
+    }
+
+    /**
+     * @param department the department to set
+     */
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    /**
+     * @return List<HourContract> return the contracts
+     */
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract){
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month){
+        double sum = baseSalary;
+        for(HourContract c : contracts){
+            int c_year = c.getDate().getMonthValue();
+            int c_month = c.getDate().getYear();
+                if(c_month == month && c_year == year){
+                    sum += c.totalValue();
+                }
+        }
+        return sum;
+    }
+}
